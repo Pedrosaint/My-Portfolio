37dfbecc2f0a5478f0c1f105c4104906d13e4309
Pedro <86964318+Pedrosaint@users.noreply.github.com>
2025-01-09 14:24:11 -0800

    Remove letter g from Contact.jsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -24,7 +24,7 @@ const Contact: React.FC = () => {
           <div className="flex items-center space-x-6">
             <div className="md:w-[6.5rem] md:h-[6.5rem] w-[5rem] h-[5rem] flex items-center justify-center rounded-full bg-[#55e6a5]">
               <DevicePhoneMobileIcon className="md:w-[4rem] md:h-[4rem] w-[3.5rem] h-[3.5rem] text-black" />
-            </div>g
+            </div>
             <div>
               <h1 className="text-[25px] mb-[0.2rem] font-semibold text-[white]">
                 Phone
